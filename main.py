dc1d0ad115d83ba76552dcc8397d888fe83cbef4
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-01-21 18:29:54 +0100

    dodano funkcje2

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def funkcja1():
     for i in range(10):
         print(f"{i}")
 
+
+def funkcja2(min: int, max: int):
+    for i in range(min, max):
+        print(f"{i ** 2}")
+
+
 if __name__ == '__main__':
     print('PyCharm')
 
     funkcja1()
-
+    funkcja2(0, 10)
