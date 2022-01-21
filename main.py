1367df09a212589da6a33a3f544b7e218caf634a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-01-21 18:39:31 +0100

    cd ..

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def funkcja2(min: int, max: int):
     for i in range(min, max):
         print(f"{i ** 2}")
 
+#proszę napisać funkcję wyśwetlającą zdefiniowane potęgi liczb dla pewnego zagresu np. 5 potęgi liczb od 2 do 100.
+
+def powRange(pow: int ,min: int, max: int):
+    pass
+
 
 if __name__ == '__main__':
     print('PyCharm')
