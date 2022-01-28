0697d00e8bc31063a6fb37763e151718e69b1c98
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-01-28 18:48:46 +0100

    macierze cd ..

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import timeit
 
+
 def funkcja1():
     for i in range(10):
         print(f"{i}")
@@ -23,28 +24,30 @@ def powRange(pow: int, min: int, max: int):
 def powRange_numpy(pow: int, min: int, max: int):
     print(np.arange(min, max, dtype=np.float32) ** pow)
 
-def arangeMatrix(min: int, max: int)->list:
+
+def arangeMatrix(min: int, max: int) -> list:
+    A = []
+    for i in range(min, max):
+        A.append(i)
+    return A
+
+
+def arangeMatrixNumpy(min: int, max: int) -> np.ndarray:
     pass
 
-def arangeMatrixNumpy(min: int, max: int)->np.ndarray:
-    pass
 
 if __name__ == '__main__':
-    #A = [1,2,3,4,5,6]
+    # A = [1,2,3,4,5,6]
 
-    #print(A)
-    #B = np.array([1,2,3,4,5,6])
-    #print(B)
-    #print(B+10)
+    # print(A)
+    # B = np.array([1,2,3,4,5,6])
+    # print(B)
+    # print(B+10)
 
-    A = []
-    for i in range(1,100_000):
-        A.append(i)
-
-    M = np.arange(1,100_000_000,2)
+    M = np.arange(1, 100_000_000, 2)
     print(M[10000])
 
     start_time = timeit.default_timer()
+    arangeMatrix(0, 10_000_000)
     end_time = timeit.default_timer()
-
-    print(f"arangeMatrix elapsed time: {(end_time-start_time):0.5f}")
+    print(f"arangeMatrix elapsed time: {(end_time - start_time):0.5f}s")
