bbeeb748779f78284625ff6cdde1ed35e04a93f1
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-01-28 18:57:30 +0100

    macierze cd ..

--- main.py
+++ main.py
@@ -46,12 +46,15 @@ if __name__ == '__main__':
 
     M = np.arange(1, 100_000_000, 2)
 
+    min = 0
+    max = 10_000_000
+
     start_time = timeit.default_timer()
-    arangeMatrix(0, 10_000_000)
+    arangeMatrix(min, max)
     end_time = timeit.default_timer()
     print(f"arangeMatrix elapsed time: {(end_time - start_time):0.5f}s")
 
     start_time = timeit.default_timer()
-
+    arangeMatrixNumpy(min, max)
     end_time = timeit.default_timer()
     print(f"arangeMatrixNumpy elapsed time: {(end_time - start_time):0.5f}s")
