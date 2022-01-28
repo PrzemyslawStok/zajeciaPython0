1c53be5093ac7429e4f1b5d2889ae5405bb14a71
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-01-28 18:27:27 +0100

    macierze cd ..

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import timeit
 
 def funkcja1():
     for i in range(10):
@@ -22,11 +23,23 @@ def powRange(pow: int, min: int, max: int):
 def powRange_numpy(pow: int, min: int, max: int):
     print(np.arange(min, max, dtype=np.float32) ** pow)
 
+def arangeMatrix(min: int, max: int)->list:
+    pass
+
+def arangeMatrixNumpy(min: int, max: int)->np.ndarray:
+    pass
 
 if __name__ == '__main__':
-    A = [1,2,3,4,5,6]
+    #A = [1,2,3,4,5,6]
 
-    print(A)
-    B = np.array([1,2,3,4,5,6])
-    print(B)
-    print(B+10)
+    #print(A)
+    #B = np.array([1,2,3,4,5,6])
+    #print(B)
+    #print(B+10)
+
+    A = []
+    for i in range(1,100_000):
+        A.append(i)
+
+    M = np.arange(1,100_000_000,2)
+    print(M[10000])
